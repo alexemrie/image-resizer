a5bec3e135cad6b81dbb3fd0ee86a17d373c7f7c
Alex Emrie <emrieaj@gmail.com>
2019-02-18 11:36:06 -0700

    refactor home.js into separate functions, single click for resizing image working

--- home.js
+++ home.js
@@ -1,44 +1,75 @@
-function resizeImage() {
-  var filesToUpload = document.getElementById('imageFile').files;
-  var file = filesToUpload[0];
+window.onload = setup;
 
-  // Create an image
-  var img = document.createElement("img");
-  // Create a file reader
-  var reader = new FileReader();
-  // Set the image once loaded into file reader
-  reader.onload = function(e) {
-          img.src = e.target.result;
+function setup() {
+    // attach click handler to imageResizer input
+    document.getElementById("imageResizer").addEventListener("click", retrieveImageInput);
+}
 
-          var canvas = document.createElement("canvas");
-          var ctx = canvas.getContext("2d");
-          ctx.drawImage(img, 0, 0);
+function retrieveImageInput() {
+    let file = getImageFile();
+    readImageFile(file);
+}
 
-          var MAX_WIDTH = 100;
-          var MAX_HEIGHT = 100;
-          var width = img.width;
-          var height = img.height;
+function getImageFile() {
+    var filesToUpload = document.getElementById('imageFile').files;
+    var file = filesToUpload[0];
+    return file;
+}
 
-          // set proper ratio of width to height using maximums
-          if (width > height) {
-              if (width > MAX_WIDTH) {
-                  height *= MAX_WIDTH / width;
-                  width = MAX_WIDTH;
-              }
-          } else {
-              if (height > MAX_HEIGHT) {
-                  width *= MAX_HEIGHT / height;
-                  height = MAX_HEIGHT;
-              }
-          }
-          canvas.width = width;
-          canvas.height = height;
-          var ctx = canvas.getContext("2d");
-          ctx.drawImage(img, 0, 0, width, height);
+function readImageFile(imageFile) {
+    var reader = new FileReader();
+    reader.readAsDataURL(imageFile);
 
-          var dataurl = canvas.toDataURL("image/png");
-          document.getElementById('output').src = dataurl;
-      }
-      // Load files into file reader
-  reader.readAsDataURL(file);
+    // Set the image once loaded into file reader
+    reader.onload = (e) => createNewImage(e);
+}
+
+function createNewImage(e) {
+    // Create an image
+    var img = document.createElement("img");
+    img.src = e.target.result;
+
+    // Wait for image element to load on page
+    img.addEventListener('load', () => {
+        // Create a canvas
+        var canvas = document.createElement("canvas");
+
+        /*
+            Wait for canvas element to load
+                Call drawAndResizeCanvas setting 'this' as new canvas object explicitly
+                Pass new image as argument
+        */
+        canvas.addEventListener('load', drawAndResizeCanvas.apply(canvas, [img]));
+    })
+}
+
+function drawAndResizeCanvas(img) {
+    canvas = this;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var MAX_WIDTH = 100;
+    var MAX_HEIGHT = 100;
+    var width = img.width;
+    var height = img.height;
+
+    // set proper ratio of width to height using maximums
+    if (width > height) {
+        if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+        }
+    } else {
+        if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+        }
+    }
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, width, height);
+
+    var dataurl = canvas.toDataURL("image/png");
+    document.getElementById('output').src = dataurl;
 }
